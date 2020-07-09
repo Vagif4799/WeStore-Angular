2be2ebe3362b05fe1eb79870495019b9277a7315
vquliyev <vagifguliyev99@gmail.com>
2020-07-09 20:02:12 +0400

    --Product Service/Class/Component added

---
+++ WeStore-Angular/src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import {ProductService} from '../../services/product.service';
+import {ActivatedRoute} from '@angular/router';
+import {Product} from '../../common/product';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  products: Product[];
+  currentCategoryId: number;
+
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(
+      () => {
+        this.listProducts();
+      }
+    );
+  }
+
+
+  listProducts() {
+
+    // check if "id" parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+    } else {
+      this.currentCategoryId = 1;
+    }
+
+    this.productService.getProductList(this.currentCategoryId).subscribe(
+      data => {
+        this.products = data;
+      }
+    );
+  }
+
+}
