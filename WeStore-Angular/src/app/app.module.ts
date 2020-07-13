38ae257165608c506f451e4379d76cd04304f7bb
vquliyev <vagifguliyev99@gmail.com>
2020-07-13 21:40:54 +0400

    --Product Details Component added

--- WeStore-Angular/src/app/app.module.ts
+++ WeStore-Angular/src/app/app.module.ts
@@ -8,6 +8,7 @@ import {HttpClientModule} from '@angular/common/http';
 import {ProductService} from './services/product.service';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { SearchComponent } from './components/search/search.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 const routes: Routes = [
   {path: 'search/:keyword', component: ProductListComponent},
@@ -23,7 +24,8 @@ const routes: Routes = [
     AppComponent,
     ProductListComponent,
     ProductCategoryMenuComponent,
-    SearchComponent
+    SearchComponent,
+    ProductDetailsComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
