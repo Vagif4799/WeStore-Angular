edf6da733139a3e3f428862a689ac2f7828af313
vquliyev <vagifguliyev99@gmail.com>
2020-07-18 17:11:32 +0400

    --reactive forms module support added

--- WeStore-Angular/src/app/app.module.ts
+++ WeStore-Angular/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ProductDetailsComponent } from './components/product-details/product-de
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 const routes: Routes = [
   {path: 'checkout', component: CheckoutComponent},
@@ -39,7 +40,8 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes),
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
