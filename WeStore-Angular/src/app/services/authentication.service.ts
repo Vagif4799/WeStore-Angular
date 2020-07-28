1545a975bc7de032de1fb7d4624f00d3c41bcf23
vquliyev <vagifguliyev99@gmail.com>
2020-07-28 18:32:18 +0400

    --Login service added

--- WeStore-Angular/src/app/services/authentication.service.ts
+++ WeStore-Angular/src/app/services/authentication.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import {HttpClient, HttpErrorResponse, HttpResponse} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {User} from '../common/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  private host = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(user: User): Observable<HttpResponse<any> | HttpErrorResponse> {
+    return this.http.post<HttpResponse<any> | HttpErrorResponse>
+    (`${this.host}/user/login`, user, {observe: 'response'});
+  }
+
+  register(user: User): Observable<HttpResponse<User> | HttpErrorResponse> {
+    return this.http.post<HttpResponse<User> | HttpErrorResponse>
+    (`${this.host}/user/register`, user);
+  }
 }
