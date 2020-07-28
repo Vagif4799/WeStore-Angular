5e20d30ea9d1858c11acade0a888d8513e162db4
vquliyev <vagifguliyev99@gmail.com>
2020-07-28 19:10:54 +0400

    --add user service

--- WeStore-Angular/src/app/services/user.service.ts
+++ WeStore-Angular/src/app/services/user.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import {HttpClient, HttpErrorResponse, HttpResponse} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {User} from '../common/user';
+import {Form} from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private host = environment.apiUrl;
 
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<User[] | HttpErrorResponse> {
+    return this.http.get<User[]>(`${this.host}/user/list`);
+  }
+
+  addUser(formData: FormData): Observable<User | HttpErrorResponse> {
+    return this.http.post<User>(`${this.host}/user/add`, formData);
+  }
 }
